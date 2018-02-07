0d027bb93b29eb0483f02d3ab3a3e3958e1e1d15
Jeremy Legendre <jslegendre@gmail.com>
2018-02-06 20:58:04 -0500

    Update main.m

--- NSNotification/main.m
+++ NSNotification/main.m
@@ -9,17 +9,18 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
         Class bundle = objc_getClass("NSBundle");
-        class_replaceMethod(bundle, @selector(bundleIdentifier), imp_implementationWithBlock(^ {return @"com.apple.finder";}), NULL);
+        class_replaceMethod(bundle, @selector(bundleIdentifier), 
+            imp_implementationWithBlock(^ {
+                return @"com.apple.finder";
+            }), NULL);
         NSUserNotification *n = [[NSUserNotification alloc] init];
-        NSUserNotificationCenter *defCenter = [NSUserNotificationCenter defaultUserNotificationCenter];
         n.title = @"Title";
         n.subtitle = @"Subtitle";
         n.informativeText = @"Informative Text";
-        [defCenter deliverNotification:n];
-        [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:3.0]];
-    }
-    
+        [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:n];
+        [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+        
+    }    
     return 0;
 }
